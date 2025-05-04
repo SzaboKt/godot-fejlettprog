060f77bd1ea16271fc008963f93115177093ca00
SzaboKt <sz.katalin.00528@gmail.com>
2025-05-04 14:37:17 +0200

    feat: added double swiper

--- level/swiper/scenes/animated_short_swiper.gd
+++
@@ -1,8 +0,0 @@
-extends Node3D
-
-@export var speed_scale: float = 1.0
-@export var animation_player: AnimationPlayer
-
-func _ready() -> void:
-	animation_player.play("swipe")
-	animation_player.speed_scale = speed_scale
