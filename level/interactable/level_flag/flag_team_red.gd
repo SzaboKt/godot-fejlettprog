a2231c04e7512c2289f6cc2f9b2bac6cdd2e0836
SzaboKt <sz.katalin.00528@gmail.com>
2025-05-04 22:53:37 +0200

    feat: added red flag

---
+++ level/interactable/level_flag/flag_team_red.gd
@@ -0,0 +1,9 @@
+extends StaticBody3D
+
+@onready var _area_3d: Area3D = %Area3D
+
+
+func _ready() -> void:
+	_area_3d.body_entered.connect(func (_body_that_entered: PhysicsBody3D) -> void:
+		Events.flag_reached.emit()
+	)
