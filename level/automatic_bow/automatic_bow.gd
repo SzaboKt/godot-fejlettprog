0cca989f2936667c1cadf58f0957a4cab2e44ed3
SzaboKt <sz.katalin.00528@gmail.com>
2025-05-13 13:15:56 +0200

    chore: reorganized level files and scenes

--- level/automatic_bow/automatic_bow.gd
+++
@@ -1,9 +0,0 @@
-extends Node3D
-
-
-@onready var projectile_launcher: ProjectileLauncher = $ProjectileLauncher
-
-@export var wait_time: float = 0
-
-func _ready() -> void:
-	projectile_launcher.wait_time = wait_time
